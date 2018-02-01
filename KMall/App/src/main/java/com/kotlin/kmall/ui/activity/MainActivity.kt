b790d17f97114ce6bdbadc40ad85fc50a64439db
jiangtingting <18792898826@163.com>
2018-02-01 22:48:17 +0800

    init

---
+++ KMall/App/src/main/java/com/kotlin/kmall/ui/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.kotlin.kmall.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.kmall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
